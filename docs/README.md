88052d78639bff49b08d8a9d51b4cf3c2f9acf45
Benjamin Sams <administrator@administrators-MacBook-Pro.local>
2022-05-13 13:27:15 -0400

    initial-commit

---
+++ docs/README.md
@@ -0,0 +1,7 @@
+# Working-With-Strings
+
+> This Python project contains two functions:
+>
+>*letter_frequency* will take in a word to check, and a letter to check for. It will return an integer denoting the number of times the *'letter'* appears in the *'word'*.
+>
+> *word_length* will take in text as input, and output an integer denoting the average length of eath word in the *'text'*.
---
+++ main.py
@@ -0,0 +1,27 @@
+# letter_frequency will take in a word to 
+# check, and a letter to check for. It will 
+# return an integer denoting the number of 
+# times the 'letter' appears in the 'word'.
+def letter_frequency():
+  word = input()
+  letter = input()
+  n = len(word)
+  
+  per = ((word.count(letter)/n)*100)
+  
+  print(int(per))
+
+# word_length will take in text as input, and
+# output an integer denoting the average length
+# of eath word in the 'text'.
+def word_length():
+  text = input()
+  
+  z = text.split()
+  y = len(z)
+  x = 0
+  
+  for i in z:
+    x += len(i)
+  
+  print(x/y)
